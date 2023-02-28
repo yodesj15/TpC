8e7a56fc52bcc5a51bba6965ee1f7199a6927010
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2023-02-27 20:06:06 -0500

    AC

--- Tp/IEnumerateur.cs
+++ Tp/IEnumerateur.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Tp
 {
-    interface IEnumerateur<string> : IEnumerator<string>
+    interface IEnumerateur<T> : IEnumerator<string>
     {
         //La propriété Filling, dont je me sers si j'ai besoin de lignes alors que le contenu dans la boîte a été épuisé, et
         //La propriété HasNext, qui s'avère seulement si l'énumérateur ne mène pas déjà au dernier élément de la séquence d'origine
