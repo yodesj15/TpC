3d64e55e0f1e1f38f0d95f68b39348ebab5fca27
yodesj15 <102743437+yodesj15@users.noreply.github.com>
2023-04-03 20:21:10 -0400

    Update IVisiteur.cs

--- Tp/IVisiteur.cs
+++ Tp/IVisiteur.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Boites
 {
-    interface IVisiteur
+    interface IVisiteur<T>
     {
+        void Entrer();
+        void Sortir();
+        void Visiter(T elem, Action opt);
     }
 }
